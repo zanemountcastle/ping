e7998c35922ca7b6ea7fd0c852d07e8bc79e4a0d
Zane Mountcastle <zane.mountcastle@gmail.com>
2017-11-22 18:12:23 +0400

    Removed unused import

--- src/components/AppletPreview.js
+++ src/components/AppletPreview.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
 
 export default class AppletPreview extends Component {
   componentWillMount() {
