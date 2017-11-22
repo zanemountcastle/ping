4f55815e8da9f99c60a0655d3bf9214f7d03a64c
Zane Mountcastle <zane.mountcastle@gmail.com>
2017-11-22 21:59:04 +0400

    Fixed app.test.js bug

--- __tests__/containers/App.test.js
+++ __tests__/containers/App.test.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import App from '../../App';
 
-import renderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
 describe('main entrypoint', () => {
-  // it('renders without crashing', () => {
-  //   const rendered = renderer.create(<App />).toJSON();
-  //   expect(rendered).toBeTruthy();
-  // });
-
   it('renders without crashing', () => {
-    const rendered = renderer.create(<App />).toJSON();
-    expect(rendered).toMatchSnapshot();
+    // Shallow render <App /> and not it's child components
+    // react-test-renderer doesn't allow shallow rendering
+    const renderer = new ShallowRenderer();
+    expect(renderer.render(<App />)).toBeTruthy();
+  });
+
+  it('renders correctly', () => {
+    const renderer = new ShallowRenderer();
+    expect(renderer.render(<App />)).toMatchSnapshot();
   });
 });
