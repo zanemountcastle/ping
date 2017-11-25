f1b4de4790494a67b42cd6955965fb61a4adc577
Zane Mountcastle <zane.mountcastle@gmail.com>
2017-11-25 17:27:09 +0400

    Added tests for Activity screen

---
+++ __tests__/containers/Activity.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Activity from '../../src/screens/Activity';
+
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+describe('activity screen', () => {
+
+  it('renders without crashing', () => {
+    // Shallow render <App /> and not it's child components
+    // Standard react-test-renderer doesn't allow shallow rendering
+    const renderer = new ShallowRenderer();
+    expect(renderer.render(<Activity />)).toBeTruthy();
+  });
+
+  it('renders correctly', () => {
+    const renderer = new ShallowRenderer();
+    expect(renderer.render(<Activity />)).toMatchSnapshot();
+  });
+
+});
