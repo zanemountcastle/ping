a291d55cd24344efa76d62814d7170e9c916ab35
Cristhian Munoz <cmm959@nyu.edu>
2017-11-28 16:39:28 +0400

    Firebase initialization

---
+++ api/registerForPushNotificationsAsync.js
@@ -0,0 +1,47 @@
+import { Permissions, Notifications } from 'expo';
+import * as firebase from 'firebase';
+/*
+firebase.initializeApp({
+  apiKey: "AIzaSyBZGYJUNo7DuYgSUhCZ3S58zM0gO7fcYrY",
+  authDomain: "ping-fdb36.firebaseapp.com",
+  databaseURL: "https://ping-fdb36.firebaseio.com",
+  projectId: "ping-fdb36",
+  storageBucket: "ping-fdb36.appspot.com",
+  messagingSenderId: "944163924283"
+});
+*/
+
+export default (async function registerForPushNotificationsAsync() {
+  // Android remote notification permissions are granted during the app
+  // install, so this will only ask on iOS
+  let { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+
+  // Stop here if the user did not grant permissions
+  if (status !== 'granted') {
+    return;
+  }
+  // Get the token that uniquely identifies this device
+  let token = await Notifications.getExpoPushTokenAsync();
+
+
+
+  userID = firebase.auth().currentUser.uid;
+
+  firebase.database().ref('/users/' + userID).update({ token: token });
+
+
+
+  // // POST the token to our backend so we can use it to send pushes from there
+  // return fetch(PUSH_ENDPOINT, {
+  //   method: 'POST',
+  //   headers: {
+  //     Accept: 'application/json',
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     token: {
+  //       value: token,
+  //     },
+  //   }),
+  // });
+});
