a8af4c47b495c1ed13cd941c288860f471c56bdf
Cristhian Munoz <cmm959@nyu.edu>
2017-12-07 01:21:43 +0400

    Ejected. Firebase installation. Does not compile due to Expo errors

---
+++ index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Tabs } from './src/lib/Router';
+//import { AppLoading } from 'expo';
+import RootNavigation from './src/containers/RootNavigation';
+import * as firebase from 'firebase';
+//import {Notifications} from 'expo';
+
+import { Provider } from 'react-redux';
+import Store from './src/lib/Store';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBZGYJUNo7DuYgSUhCZ3S58zM0gO7fcYrY",
+  authDomain: "ping-fdb36.firebaseapp.com",
+  databaseURL: "https://ping-fdb36.firebaseio.com",
+  projectId: "ping-fdb36",
+  storageBucket: "ping-fdb36.appspot.com",
+  messagingSenderId: "944163924283"
+});
+
+export default class Ping extends React.Component {
+
+  render() {
+    return (
+      <Provider store={Store}>
+        <RootNavigation />
+      </Provider>
+    );
+  }
+}
