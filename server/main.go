e2a002f17d21c2424f5905979760323ec298c5cb
Brooke Hopkins <brooke.hopkins@nyu.edu>
2017-12-09 14:35:07 +0400

    Push to firebase successfully

--- server/main.go
+++ server/main.go
@@ -6,16 +6,106 @@ import (
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
-	"time"
+	// "time"
   "io"
   "log"
+  "strings"
+  "gopkg.in/zabawaba99/firego.v1"
+  // "golang.org/x/net/context"
+  // "firebase.google.com/go"
+// "ioutil"
+  "golang.org/x/oauth2"
+  "bytes"
+  "encoding/json"
+  "io/ioutil"
+  // "google.golang.org/api/option"
+  // "google"
+  "golang.org/x/oauth2/google"
 )
+type Payload struct {
+    Feed string
+    // title string
+    // body string
+    // feedId int32
+    // creatorID int32
+}
 
 // Job holds the attributes needed to perform unit of work.
 type Job struct {
-	Name  string
-	Delay time.Duration
+	Payload Payload
 }
+
+func (p *Payload) UploadToFB() error {
+
+  // f := firego.New("https://ping-fdb36.firebaseIO.com", nil)
+
+  fmt.Printf("Create Firebase\n")
+fmt.Printf("Read File\n")
+  d, err := ioutil.ReadFile("ping-fdb36-firebase-adminsdk-plh3v-282c5f84fb.json")
+  if err != nil {
+
+      fmt.Printf("Error\n")
+      return err
+  }
+fmt.Printf("Config JSON\n")
+  conf, err := google.JWTConfigFromJSON(d, "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/firebase.database")
+  if err != nil {
+      fmt.Printf("Error\n")
+      return err
+  }
+  fmt.Printf("firego\n")
+  fb := firego.New("https://ping-fdb36.firebaseio.com", conf.Client(oauth2.NoContext))
+  // use the authenticated fb instance
+
+  // opt := option.WithCredentialsFile("ping-fdb36-firebase-adminsdk-plh3v-282c5f84fb.json")
+  // app, err := firebase.NewApp(context.Background(), nil, opt)
+  // if err != nil {
+  //         log.Fatalf("error initializing app: %v\n", err)
+  // }
+
+  // the storageFolder method ensures that there are no name collision in
+  // case we get same timestamp in the key name
+  //  storage_path := fmt.Sprintf("%v/%v", p.storageFolder, time.Now().UnixNano())
+
+  fmt.Printf("bytes\n")
+	b := new(bytes.Buffer)
+  fmt.Printf("json\n")
+	encodeErr := json.NewEncoder(b).Encode(p)
+
+
+	if encodeErr != nil {
+    fmt.Printf("Error\n")
+		return encodeErr
+	}
+
+
+  fmt.Printf("push\n")
+
+   fb, err = fb.Ref("/feed/"+ p.Feed)
+   pushedFirego, err := fb.Push( p.Feed)
+
+  // fmt.Printf(pushedFirego)
+
+  // fmt.Printf(err.Error())
+  if err != nil {
+
+      // fmt.Printf("Error: " + err.Error())
+  	return err;
+  }
+
+  var bar string
+  if err := pushedFirego.Value(&bar); err != nil {
+    // fmt.Printf("Error: " + err.Error())
+  	return err
+  }
+
+  // // prints "https://my-firebase-app.firebaseIO.com/-JgvLHXszP4xS0AUN-nI: bar"
+  // fmt.Printf("%s: %s\n", pushedFirego, bar)
+
+	return err
+}
+
 
 // NewWorker creates takes a numeric id and a channel w/ worker pool.
 func NewWorker(id int, workerPool chan chan Job) Worker {
@@ -42,10 +132,16 @@ func (w Worker) start() {
 
 			select {
 			case job := <-w.jobQueue:
+          fmt.Printf("Starting job %s",job.Payload)
 				// Dispatcher has added a job to my jobQueue.
-				fmt.Printf("worker%d: started %s, blocking for %f seconds\n", w.id, job.Name, job.Delay.Seconds())
-				time.Sleep(job.Delay)
-				fmt.Printf("worker%d: completed %s!\n", w.id, job.Name)
+				// fmt.Printf("worker%d: started %s, blocking for %f
+// seconds\n", w.id, job.Delay.Seconds())
+				// time.Sleep(job.Delay)
+
+				// fmt.Printf("worker%d: completed %s!\n", w.id, job.
+// Name)  fmt.Print
+          job.Payload.UploadToFB()
+          fmt.Printf("Finished uploading%s",job.Payload)
 			case <-w.quitChan:
 				// We have been asked to stop.
 				fmt.Printf("worker%d stopping\n", w.id)
@@ -92,9 +188,9 @@ func (d *Dispatcher) dispatch() {
 		select {
 		case job := <-d.jobQueue:
 			go func() {
-				fmt.Printf("fetching workerJobQueue for: %s\n", job.Name)
+				fmt.Printf("fetching workerJobQueue for: \n")
 				workerJobQueue := <-d.workerPool
-				fmt.Printf("adding %s to workerJobQueue\n", job.Name)
+				fmt.Printf("adding to workerJobQueue\n")
 				workerJobQueue <- job
 			}()
 		}
@@ -104,35 +200,40 @@ func (d *Dispatcher) dispatch() {
 func requestHandler(w http.ResponseWriter, r *http.Request, jobQueue chan Job) {
 	// Make sure we can only be called with an HTTP POST request.
 
-  io.WriteString(w, "Hello world!")
-  if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
+  // io.WriteString(w, "Hello world!")
+    // io.WriteString(w, r.URL.Path)
+  // if r.Method != "POST" {
+	// 	w.Header().Set("Allow", "POST")
+	// 	w.WriteHeader(http.StatusMethodNotAllowed)
+	// 	return
+	// }
+  // Parse feed
 
-	// Parse the delay.
-	delay, err := time.ParseDuration(r.FormValue("delay"))
-	if err != nil {
-		http.Error(w, "Bad delay value: "+err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// Validate delay is in range 1 to 10 seconds.
-	if delay.Seconds() < 1 || delay.Seconds() > 10 {
-		http.Error(w, "The delay must be between 1 and 10 seconds, inclusively.", http.StatusBadRequest)
-		return
-	}
-
-	// Set name and validate value.
-	name := r.FormValue("name")
-	if name == "" {
-		http.Error(w, "You must specify a name.", http.StatusBadRequest)
-		return
-	}
+  feed := strings.TrimPrefix(r.URL.Path, "/")
+  io.WriteString(w, feed)
+	// // Parse the delay.
+	// delay, err := time.ParseDuration(r.FormValue("delay"))
+	// if err != nil {
+	// 	http.Error(w, "Bad delay value: "+err.Error(), http.StatusBadRequest)
+	// 	return
+	// }
+  //
+	// // Validate delay is in range 1 to 10 seconds.
+	// if delay.Seconds() < 1 || delay.Seconds() > 10 {
+	// 	http.Error(w, "The delay must be between 1 and 10 seconds, inclusively.", http.StatusBadRequest)
+	// 	return
+	// }
+  //
+	// // Set name and validate value.
+	// name := r.FormValue("name")
+	// if name == "" {
+	// 	http.Error(w, "You must specify a name.", http.StatusBadRequest)
+	// 	return
+	// }
 
 	// Create Job and push the work onto the jobQueue.
-	job := Job{Name: name, Delay: delay}
+  p := Payload{Feed: feed}
+	job := Job{Payload: p}
 	jobQueue <- job
 
 	// Render success.
